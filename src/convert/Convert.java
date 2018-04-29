f890bf26758f773ca9512b8da5ed627066a16837
Klinikova <klinikova@gmail.com>
2018-04-28 21:16:17 -0700

    init commit

---
+++ src/convert/Convert.java
@@ -0,0 +1,49 @@
+package convert;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author PolinaYoga
+ */
+public class Convert {
+
+    public static void main(String[] args) {
+        char b;
+        char i;
+        char g;
+        char l;
+        char t;
+        char e;  
+        
+        System.out.print(Character.toUpperCase ('b'));
+        System.out.print(Character.toUpperCase('i'));
+        System.out.println(Character.toUpperCase('g'));
+        
+        System.out.print(Character.toLowerCase ('l'));
+        System.out.print(Character.toLowerCase ('i'));
+        System.out.print(Character.toLowerCase ('t'));
+        System.out.print(Character.toLowerCase ('t'));
+        System.out.print(Character.toLowerCase ('l'));
+        System.out.println(Character.toLowerCase ('e'));
+       
+        /**
+                Write an if statement that displays the word “digit” if the char variable ch contains
+a numeric digit. Otherwise, it should display “Not a digit.” */
+        
+        if ()
+            {
+            System.out.print(Character.isDigit(e));
+            }
+        else
+            System.out.print(Character.isDigit(e));
+        
+        Scanner keyboard = new Scanner(System.in);
+        
+        System.out.println(Character.toUpperCase(Character.toLowerCase('A')));
+        
+        
+        System.out.println("Do you want to repeat the program or quit? (R/Q)");
+    }
+    
+}
